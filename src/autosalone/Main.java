5b8847fea3c4a7a5b85e7ff0476905aeb4275f2f
AlessandroPica <pica.alessandro.studente@itispaleocapa.it>
2024-09-30 13:18:57 +0200

    programmazione metodo main

--- src/autosalone/Main.java
+++ src/autosalone/Main.java
@@ -1,4 +1,5 @@
 package autosalone;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args){
@@ -6,6 +7,41 @@ public class Main {
         Autosalone a2 = new Autosalone();
         Autosalone a3 = new Autosalone();
         Autosalone a4 = new Autosalone();
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("inserisci la marca della prima auto: ");
+        String marca1 = in.next();
+        a1.setMarca(marca1);
+        System.out.println("inserisci il modello della prima auto: ");
+        String modello1 = in.next();
+        a1.setModello(modello1);
+        System.out.println("inserisci il prezzo della prima auto: ");
+        double prezzo1 = in.nextDouble();
+        a1.setPrezzo(prezzo1);
+
+        System.out.println("inserisci la marca della seconda auto: ");
+        marca1 = in.next();
+        a2.setMarca(marca1);
+        System.out.println("inserisci il modello della seconda auto: ");
+        modello1 = in.next();
+        a2.setModello(modello1);
+        System.out.println("inserisci il prezzo della seconda auto: ");
+        prezzo1 = in.nextDouble();
+        a2.setPrezzo(prezzo1);
+
+        System.out.println("inserisci la marca della terza auto: ");
+        marca1 = in.next();
+        a3.setMarca(marca1);
+        System.out.println("inserisci il modello della terza auto: ");
+        modello1 = in.next();
+        a3.setModello(modello1);
+        System.out.println("inserisci il prezzo della terza auto: ");
+        prezzo1 = in.nextDouble();
+        a3.setPrezzo(prezzo1);
+
+
+        System.out.println("la prima auto è: "+ a1.getMarca() +" "+ a1.getModello() +" "+ a1.getPrezzo());
+        System.out.println("la seconda auto è: "+ a2.getMarca() +" "+ a2.getModello() +" "+ a2.getPrezzo());
+        System.out.println("la terza auto è: "+ a3.getMarca() +" "+ a3.getModello() +" "+ a3.getPrezzo());
     }
 }
